3875a645f2411ad5c1754dd79c874504e1ce99ae
Till Kolditz <Till.Kolditz@GoogleMail.com>
2013-02-13 15:08:26 +0100

    added comments
    Signed-off-by: Till Kolditz <Till.Kolditz@GoogleMail.com>

--- de.kolditz.common/src/de/kolditz/common/concurrent/Scheduler.java
+++ de.kolditz.common/src/de/kolditz/common/concurrent/Scheduler.java
@@ -37,12 +37,11 @@ public final class Scheduler
     {
     }
 
-    private static final Object lock;
+    private static final Object lock = new Object();
     private static volatile ScheduledThreadPoolExecutor executor;
 
     static
     {
-        lock = new Object();
         checkState();
     }
 
@@ -76,72 +75,126 @@ public final class Scheduler
         }
     }
 
+    /**
+     * Initiates an orderly shutdown in which previously submitted tasks are executed. Synchronized.
+     * 
+     * @see ScheduledThreadPoolExecutor#shutdown()
+     */
     public static void shutdown()
     {
-        executor.shutdown();
+        synchronized (lock)
+        {
+            if (executor != null)
+                executor.shutdown();
+            executor = null;
+        }
     }
 
+    /**
+     * Attempts to stop all actively executing tasks, halts the processing of waiting tasks, and returns a list of the
+     * tasks that were awaiting execution.
+     * 
+     * @return list of tasks that never commenced execution. <b>May be empty or null</b>
+     * @see ScheduledThreadPoolExecutor#shutdownNow()
+     */
     public static List<Runnable> shutdownNow()
     {
-        List<Runnable> notShutdown = executor.shutdownNow();
-        executor = null;
+        List<Runnable> notShutdown = null;
+        synchronized (lock)
+        {
+            if (executor != null)
+            {
+                notShutdown = executor.shutdownNow();
+                executor = null;
+            }
+        }
         return notShutdown;
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#purge()
+     */
     public static void purge()
     {
         checkState();
         executor.purge();
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#isShutdown()
+     */
     public static boolean isShutdown()
     {
         checkState();
         return executor.isShutdown();
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#isTerminated()
+     */
     public static boolean isTerminated()
     {
         checkState();
         return executor.isTerminated();
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#isTerminating()
+     */
     public static boolean isTerminating()
     {
         checkState();
         return executor.isTerminating();
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#awaitTermination(long, TimeUnit)
+     */
     public static boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException
     {
         checkState();
         return executor.awaitTermination(timeout, unit);
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#submit(Callable)
+     */
     public static <T> Future<T> submit(Callable<T> task)
     {
         checkState();
         return executor.submit(task);
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#submit(Runnable, Object)
+     */
     public static <T> Future<T> submit(Runnable task, T result)
     {
         checkState();
         return executor.submit(task, result);
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#submit(Runnable)
+     */
     public static Future<?> submit(Runnable task)
     {
         checkState();
         return executor.submit(task);
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#invokeAll(Collection)
+     */
     public static <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException
     {
         checkState();
         return executor.invokeAll(tasks);
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#invokeAll(Collection, long, TimeUnit)
+     */
     public static <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
             throws InterruptedException
     {
@@ -149,6 +202,9 @@ public final class Scheduler
         return executor.invokeAll(tasks, timeout, unit);
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#invokeAny(Collection)
+     */
     public static <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException,
             ExecutionException
     {
@@ -156,6 +212,9 @@ public final class Scheduler
         return executor.invokeAny(tasks);
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#invokeAny(Collection, long, TimeUnit)
+     */
     public static <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
             throws InterruptedException, ExecutionException, TimeoutException
     {
@@ -163,30 +222,45 @@ public final class Scheduler
         return executor.invokeAny(tasks, timeout, unit);
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#execute(Runnable)
+     */
     public static void execute(Runnable command)
     {
         checkState();
         executor.execute(command);
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#schedule(Runnable, long, TimeUnit)
+     */
     public static ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit)
     {
         checkState();
         return executor.schedule(command, delay, unit);
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#schedule(Runnable, long, TimeUnit)
+     */
     public static <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit)
     {
         checkState();
         return executor.schedule(callable, delay, unit);
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#scheduleAtFixedRate(Runnable, long, long, TimeUnit)
+     */
     public static ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit)
     {
         checkState();
         return executor.scheduleAtFixedRate(command, initialDelay, period, unit);
     }
 
+    /**
+     * @see ScheduledThreadPoolExecutor#scheduleWithFixedDelay(Runnable, long, long, TimeUnit)
+     */
     public static ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay,
             TimeUnit unit)
     {
